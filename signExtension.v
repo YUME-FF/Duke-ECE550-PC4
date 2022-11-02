88779d4d11466f43cef5267967b58d9d956affd4
Zihan Sheng <zihansheng@gmail.com>
2022-11-02 14:07:29 -0400

    Fix a typo in a loop

--- signExtension.v
+++ signExtension.v
@@ -1,7 +1,7 @@
 module signExtension(immed, out);
 	input[16:0] immed;
 	output[31:0] out;
-	
+
 	// First 17 bits
 	genvar i;
 	generate
@@ -9,11 +9,11 @@ module signExtension(immed, out);
 			and and1(out[i], immed[i], immed[i]);
 		end
 	endgenerate
-	
+
 	// Last 15 bits
 	genvar j;
 	generate
-		for(i = 17; i <= 31; j = j + 1) begin: for2
+		for(j = 17; j <= 31; j = j + 1) begin: for2
 			and and2(out[j], immed[16], immed[16]);
 		end
 	endgenerate
