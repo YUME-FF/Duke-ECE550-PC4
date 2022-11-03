5334ff12c2c2b22229e43ccc033127deead60375
Zihan Sheng <zihansheng@gmail.com>
2022-11-02 23:24:55 -0400

    Update testbench

--- processor_tb.v
+++ processor_tb.v
@@ -22,7 +22,7 @@ module processor_tb();
 	wire [4:0] ctrl_readRegB;           // O: Register to read from port B of regfile
 	wire [31:0] data_writeReg;          // O: Data to write to for regfile
 	reg [31:0] data_readRegA;           // I: Data from port A of regfile
-	reg [31:0] data_readRegB;            // I: Data from port B of regfile
+	reg [31:0] data_readRegB;           // I: Data from port B of regfile
 
 	// Tracking the number of errors
 	integer errors;
@@ -46,10 +46,34 @@ module processor_tb();
 		reset = 1'b0; // de-assert reset
 		@(negedge clock); // wait until next negative edge of clock
 
-		// TODO: Test imem
-		
-		// TODO: Test dmem
-		
+		// TODO: test add
+		// add $t0, $t4, $t7
+		// 00000001100011110100000000100000
+		// TODO: test addi
+		// addi $t1, $t2, 0x0035 (53)
+		// 00101001010010010000000000110101
+		// TODO: test sub
+		// sub $2, $3, $4
+		// 00000000011001000001000000100010
+		// TODO: test and
+		// and $5, $6, $7
+		// 00000000110001110010100000100100
+		// TODO: test or
+		// or $16, $17, $18
+		// 00000010001100101000000000100101
+		// TODO: test sll
+		// sll $19, $20, 3
+		// 00000000000101001001100011000000
+		// TODO: test sra
+		// sra $21, $22, 2
+		// 00000000000101101010100010000011
+		// TODO: test lw
+		// lw $23, 0x0001($27)
+		// 10001111011101110000000000000001
+		// TODO: test sw
+		// sw $25, 0x0001($30)
+		// 10101111110110010000000000000001
+
 		// Test regFile
 		for(index = 0; index <= 31; index = index + 1) begin
 			//writeRegister(index, 32'h0000DEAD);
@@ -77,12 +101,13 @@ module processor_tb();
 	always
 		#10     clock = ~clock; // toggle
 
-	// Functions for Imem testing
+	// Task for checking add operation
+	task check_add;
+		input [31:0] instr;
+	endtask
 
-	// Functions for dmem testing
-
-	// Functions for regfile testing
-	task writeRegister;
+	// Task for regfile testing
+	task check_sw;
 		input [4:0] writeReg;
 		input [31:0] value;
 
