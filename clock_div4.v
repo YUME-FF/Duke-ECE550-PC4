05a75708d63caa25d4d580e8b6975575376a7b00
YUME-FF <73271231+YUME-FF@users.noreply.github.com>
2022-11-10 16:42:33 -0500

    Update clock_div4.v

--- clock_div4.v
+++ clock_div4.v
@@ -5,15 +5,15 @@ module clock_div4(clk, rst, out_clk);
 reg [1:0] div_cnt1;
 	always@(posedge clk or negedge rst)
 begin
-	if(!rst)
+	if(rst)
 		div_cnt1<=2'b00;
 	else
 		div_cnt1<=div_cnt1+1'b1;
 end
  
-always@(posedge clk or negedge rst)  //四分频 
-begin                                      //计数器放在外面 来实现计数   div_cnt1
-	if(!rst)                           //00 01 10 11 捕捉00和10 实现四分频
+always@(posedge clk or negedge rst)
+begin                                 
+	if(rst)                          
 		out_clk<=1'b0;
 	else if(div_cnt1==2'b00 || div_cnt1==2'b10)
 		out_clk<=~out_clk;
