36e956b1b5ddc07970e2b76b46daccd279d34447
YUME-FF <73271231+YUME-FF@users.noreply.github.com>
2022-11-10 01:16:08 -0500

    Update control_circuit.v

--- control_circuit.v
+++ control_circuit.v
@@ -1,10 +1,10 @@
-module control_circuit(opcode, Rwe, Rdst, ALUinB, ALUop, BR, DMwe, JP, Rwd, op_Rype, op_Addi, op_Sw, op_Lw);
+module control_circuit(opcode, Rwe, Rdst, ALUinB, ALUop, BR, DMwe, JP, Rwd, op_Rtype, op_Addi, op_Sw, op_Lw);
 	//input
 	input [4:0] opcode;
 	
 	//Internal wire
 	wire [4:0] opcode;
-	wire [2:0] op_Rype_Tmp;
+	wire [2:0] op_Rtype_Tmp;
 	wire [2:0] op_Addi_Tmp;
 	wire [2:0] op_Sw_Tmp;
 	wire [2:0] op_Lw_Tmp;
@@ -12,10 +12,10 @@ module control_circuit(opcode, Rwe, Rdst, ALUinB, ALUop, BR, DMwe, JP, Rwd, op_R
 	wire or_ALUinB_Tmp;
 	
 	//output
-	output Rwe, Rdst, ALUinB, ALUop, BR, DMwe, JP, Rwd, op_Rype, op_Addi, op_Sw, op_Lw;
+	output Rwe, Rdst, ALUinB, ALUop, BR, DMwe, JP, Rwd, op_Rtype, op_Addi, op_Sw, op_Lw;
 	
-	//op_Rype 00000
-	is_code is_Rtype(opcode, 5'b00000, op_Rype);
+	//op_Rtype 00000
+	is_code is_Rtype(opcode, 5'b00000, op_Rtype);
 	
 	//op_Addi 00101
 	is_code is_Addi(opcode, 5'b00101, op_Addi);
@@ -32,7 +32,7 @@ module control_circuit(opcode, Rwe, Rdst, ALUinB, ALUop, BR, DMwe, JP, Rwd, op_R
 	or or_Rwe_1(Rwe, or_Rwe_Tmp, op_Lw);
 	
 	//Rdst = Rtype
-	assign Rdst = op_Rype;
+	assign Rdst = op_Rtype;
 	
 	//ALUinB = Addi + Lw + Sw
 	or or_ALUinB_0(or_ALUinB_Tmp, op_Addi, op_Lw);
