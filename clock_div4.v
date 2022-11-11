f7bb2436cf2fdb9e774dfb897cd6d50418fda424
YUME-FF <73271231+YUME-FF@users.noreply.github.com>
2022-11-10 21:53:41 -0500

    Update clock_div4.v

--- clock_div4.v
+++ clock_div4.v
@@ -2,22 +2,22 @@ module clock_div4(clk, rst, out_clk);
   output reg out_clk;
   input clk;
   input rst;	
-reg [1:0] div_cnt1;
+	reg [1:0] count;
 	always@(posedge clk)
-begin
-	if(rst)
-		div_cnt1<=2'b00;
-	else
-		div_cnt1<=div_cnt1+1'b1;
-end
+		begin
+			if(rst)
+				count<=2'b00;
+			else
+				count<=count+1'b1;
+		end
  
-always@(posedge clk)
-begin                                 
-	if(rst)                          
-		out_clk<=1'b0;
-	else if(div_cnt1==2'b00 || div_cnt1==2'b10)
-		out_clk<=~out_clk;
-	else
-		out_clk<=out_clk;
-end
+	always@(posedge clk)
+		begin                                 
+			if(rst)                          
+				out_clk<=1'b0;
+			else if(count==2'b00 || count==2'b10)
+				out_clk<=~out_clk;
+			else
+				out_clk<=out_clk;
+		end
 endmodule
